4425d83e1d299c526a1c9661bbeaabb4fc4f3839
AKNov30 <amonthjp@gmail.com>
2024-10-28 23:30:55 +0700

    add portdb

--- src/lib/db.js
+++ src/lib/db.js
@@ -7,6 +7,7 @@ const db = await mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 export default db;
